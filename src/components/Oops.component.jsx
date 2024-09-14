0f81ed9ed15f0fd2957379649690fd0d1b06cfb7
nurzhan-132 <nurzhanmomynov01@gmail.com>
2024-09-15 00:46:57 +0500

    feat: add Oops component for handling non-existent routes

---
+++ src/components/Oops.component.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Oops = () => {
+  return (
+    <div>
+      <h1 style={{ fontSize: "xxx-large", color: "red" }}>
+        O-O-O-ps, you entered a non-existent page!
+      </h1>
+    </div>
+  );
+};
+
+export default Oops;
