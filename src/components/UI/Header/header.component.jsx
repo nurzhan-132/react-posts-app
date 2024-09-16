d199b51abf3fea9ba8527ae33c8dc822fb8fa974
nurzhan-132 <nurzhanmomynov01@gmail.com>
2024-09-17 01:48:55 +0500

    feat(login logic: simple login logic illustration for showing public/private routes)

--- src/components/UI/Header/header.component.jsx
+++ src/components/UI/Header/header.component.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import styles from "./header.module.scss";
+import { AuthContext } from "../../../context";
+import Button from "../Button/button.component";
 
 const Header = () => {
+  const { isAuth, setIsAuth } = useContext(AuthContext);
+
+  const logout = () => {
+    setIsAuth(false);
+    localStorage.clear();
+  };
+
   return (
     <header className={styles["navbar"]}>
       <nav className={styles["navbar-container"]}>
         <Link to="/about">About</Link>
         <Link to="/posts">Posts</Link>
+        {isAuth && <Button onClick={logout}>Logout</Button>}
       </nav>
     </header>
   );
