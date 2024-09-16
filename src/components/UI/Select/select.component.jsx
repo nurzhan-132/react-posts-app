374b6f1a86f8994cbfbf8a767b7bda5bc71f44c1
nurzhan-132 <nurzhanmomynov01@gmail.com>
2024-09-17 01:50:35 +0500

    fix(minor changes in props)

--- src/components/UI/Select/select.component.jsx
+++ src/components/UI/Select/select.component.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import styles from "./select.module.scss";
 
-const Select = ({ options, defaultValue, value, onChange }) => {
+const Select = ({ options, defaultValue, value, onChange, style }) => {
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
+    <select
+      value={value}
+      onChange={(e) => onChange(e.target.value)}
+      style={style}
+    >
       <option disabled>{defaultValue}</option>
       {options.map((option) => (
         <option key={option.value} value={option.value}>
