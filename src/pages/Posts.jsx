6a3f91c853ec7ed0b04f1a3b383cf9aefe18f9c2
nurzhan-132 <nurzhanmomynov01@gmail.com>
2024-09-17 01:52:51 +0500

    refactor: final works, logic of infinite scroll using IntersectionObserver

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import "../styles/App.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PostList from "../components/PostList/post-list.component";
 import PostForm from "../components/PostForm/post-form.component";
 import PostFilter from "../components/PostFilter/post-filter.component";
@@ -9,8 +9,9 @@ import { usePosts } from "../hooks/usePosts";
 import PostService from "../_api/PostService";
 import Skeleton from "../components/UI/Skeleton/skeleton.component";
 import { useFetching } from "../hooks/useFetching";
-import { getPagesArray, getTotalPages } from "../_utils/pages";
+import { getTotalPages } from "../_utils/pages";
 import Pagination from "../components/UI/Pagination/pagination.component";
+// import { useObserver } from "../hooks/useObserver";
 
 function Posts() {
   const [posts, setPosts] = useState([]);
@@ -19,17 +20,17 @@ function Posts() {
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
+  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const [fetchPosts, isPostsLoading, isPostsError] = useFetching(
     async (limit, page) => {
       const response = await PostService.getAllPosts(limit, page);
+      // setPosts([...posts, ...response.data]); // for the infinite loading of posts
       setPosts(response.data);
       const totalCount = response.headers["x-total-count"];
       setTotalPages(getTotalPages(totalCount, limit));
     },
   );
-
-  const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -47,7 +48,11 @@ function Posts() {
 
   useEffect(() => {
     fetchPosts(limit, page);
-  }, []);
+  }, [page, limit]);
+
+  // useObserver(lastElement, page < totalPages, isPostsLoading, () => {
+  //   setPage(page + 1);
+  // });
 
   return (
     <div className="App">
@@ -59,15 +64,21 @@ function Posts() {
 
       <PostFilter filter={filter} setFilter={setFilter} />
       {isPostsError && <h1>{isPostsError}</h1>}
-      {isPostsLoading ? (
-        <Skeleton count={10} flexDirection={"column"} height={84} />
-      ) : (
-        <PostList
-          posts={sortedAndSearchedPosts}
-          title={"Posts about JavaScript"}
-          remove={removePost}
-        />
+      <PostList
+        posts={sortedAndSearchedPosts}
+        title={"Posts about JavaScript"}
+        remove={removePost}
+        limit={limit}
+        setLimit={(val) => setLimit(val)}
+      />
+      {isPostsLoading && (
+        <Skeleton count={limit} flexDirection={"column"} height={84} />
       )}
+      {/*  Special div for observing the end of posts' list. When it is visible on the screen we load another pack of posts*/}
+      {/*<div*/}
+      {/*  ref={lastElement}*/}
+      {/*  style={{ height: "20px", background: "transparent" }}*/}
+      {/*/>*/}
       <Pagination page={page} totalPages={totalPages} changePage={changePage} />
     </div>
   );
